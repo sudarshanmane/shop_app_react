1e8fff09ee13f28f57c4e37894fda744bdef711b
sudarshanmane <sudarshanmane2110@gmail.com>
2024-12-22 20:49:38 +0530

    done

--- src/Fetch/urls.js
+++ src/Fetch/urls.js
@@ -5,11 +5,11 @@ const URLS = {
   // receipt: "/products/receipt",
   // receipt_url: "http://localhost:5001/public/receipt.pdf",
 
-  base_url: "http://kimbhlostandfound.in/shopapp/api/v1",
+  base_url: "https://kumbhlostandfound.in/shopapp/api/v1",
   login: "/products/login",
   products: "/products",
   receipt: "/products/receipt",
-  receipt_url: "http://kimbhlostandfound.in/shopapp/public/receipt.pdf",
+  receipt_url: "https://kumbhlostandfound.in/shopapp/public/receipt.pdf",
 };
 
 export default URLS;
